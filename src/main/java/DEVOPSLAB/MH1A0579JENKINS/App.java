ed4f749ba945768a996f4774a56a165113a619b6
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-12-04 13:11:54 +0530

    charan

---
+++ src/main/java/DEVOPSLAB/MH1A0579JENKINS/App.java
@@ -0,0 +1,13 @@
+package DEVOPSLAB.MH1A0579JENKINS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
